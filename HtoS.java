62de3ab0dc860df2f7e76e030698b0a6ab847813
VarunprasadT <varunprasadt.xworkz@gmail.com>
2022-10-29 10:44:18 +0530

    Second tetst files

--- HtoS.java
+++
@@ -1,7 +0,0 @@
-class HtoS{
-public static void main(String []args){
-int timeinHours= 4;
-int timeinSec = timeinHours*60*60;
-System.out.println(" Time in Hours "+timeinHours+"Hrs"+" is Converted into Seconds : "+timeinSec+"sec");
-}
-}
