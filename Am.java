f17d66ec47de6cce03e47b1b24534b630154797c
VarunprasadT <115654491+VarunprasadT@users.noreply.github.com>
2022-11-03 22:39:09 +0530

    Update Am.java

--- Am.java
+++ Am.java
@@ -2,12 +2,19 @@ class Am{
 	public static void main(String []args){
 		int num=123;
 		int r=1;
-		int sum;
-		for(int i=0;i>0;i++){
+		int sum=0;
+		for(int i=0;i<4;i++){
+			for(;num!=0;num=num/10){
 			num=num%10;
 			r=num;
-			r=(r*r*r);
+			sum +=(r*r*r);
 		}
-		System.out.println(r);
+		}
+		if(sum==num){
+			System.out.println("The number is Amstrong number");
+		}
+		else{
+			System.out.println("The number is not an Amstrong number");
+		}
 	}
 }
